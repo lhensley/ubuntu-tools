ad63502cb589e7140ed905c64c4860df4131b0af
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-05-09 16:01:38 -0500

    progress-bar.sh update

--- sbin/progress-bar.sh
+++ sbin/progress-bar.sh
@@ -12,14 +12,16 @@ DONE=$2
 let WIDTH=$(tput cols)-2
 
 function pounds () {
-for ((i = 1; i = $1; i++)); do
+for ((i = 1; i <= $1; i++)); do
     echo -ne "#"
 done
+}
 
 function spaces () {
-for ((i = 1; i = $1; i++)); do
+for ((i = 1; i <= $1; i++)); do
     echo -ne " "
 done
+}
 
 if [[ -z "$ITEMS" ]]; then
     ITEMS=0
@@ -28,13 +30,13 @@ if [[ -z "$ITEMS" ]]; then
 if [[ -z "$DONE" ]]; then
     DONE=0
     fi
-
+    .
 if [[ ${WIDTH} -lt 10 ]]; then
     exit
     fi
 
 if [[ ${ITEMS} -lt 0 ]]; then
-    DONE=0
+    ITEMS=0
     fi
 
 if [[ ${DONE} -lt 0 ]]; then
@@ -47,11 +49,7 @@ if [[ ${DONE} -gt "$ITEMS" ]]; then
 
 if [[ ${ITEMS} -lt 0 ]]; then
     DONE=0
-    fi
-
-if [[ ${WIDTH} -lt 10 ]]; then
-    exit
-    fi
+fi
 
 let ERASE=WIDTH-2
 if [[ ${ITEMS} -eq 0 ]]; then
@@ -60,21 +58,21 @@ if [[ ${ITEMS} -eq 0 ]]; then
     exit
     fi
 
-let RANGE=WIDTH-10
+let RANGE=WIDTH-6
 let POUNDS=DONE*RANGE/ITEMS
 let SPACES=RANGE-POUNDS
 let PERCENT=100*DONE/ITEMS
 
-echo "Items: " $ITEMS
-echo "Done: " $DONE
-echo "Range: " $RANGE
-echo "Pounds: " $POUNDS
-echo "Spaces: " $SPACES
-echo "Percent: " $PERCENT
-echo "Erase: " $ERASE
+# echo "Items: " $ITEMS
+# echo "Done: " $DONE
+# echo "Range: " $RANGE
+# echo "Pounds: " $POUNDS
+# echo "Spaces: " $SPACES
+# echo "Percent: " $PERCENT
+# echo "Erase: " $ERASE
 
-printf "%3d [" $PERCENT
+printf "%3d%% [" $PERCENT
 pounds $POUNDS
 spaces $SPACES
 printf "]"
-printf "\n"
+# echo ""
