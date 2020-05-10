bb7ab587ba065479121b6a71608c22c5a10a0ba6
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-05-09 21:35:13 -0500

    phpmyadmin

--- sbin/setup/after-110.sh
+++ sbin/setup/after-110.sh
@@ -61,10 +61,11 @@ fi
 # phpMyAdmin should be installed AFTER php and MySQL
 if $install_mysql_server ; then
   apt-get install -y openssl libcurl4-openssl-dev libssl-dev
-  apt-get install -y mysql-server
   MYSQL_ROOT_PASSWORD="YtMhe5rY#Qs2fFb&%n#qDtAi3k!Q3mUN"
   debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
   debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+  apt-get install -y mysql-server
+exit
   mysql_secure_installation --use-default
 #  cp /etc/mysql/mysql.conf.d/mysqld.cnf \
 #  /etc/mysql/mysql.conf.d/mysqld.cnf-$(date '+%Y%m%d%H%M%S')
