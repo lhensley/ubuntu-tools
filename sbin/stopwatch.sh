5005e7218eb46a4ea8e3044ab3d63fa895cf1e89
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-05-12 21:56:06 -0500

    Fixed .ssh

--- sbin/stopwatch.sh
+++ sbin/stopwatch.sh
@@ -3,31 +3,62 @@
 # Should have owner root:$USER_NAME
 # Should have permissions 770
 #
-$SWDIR="/tmp"
-$SW="$SWDIR/stopwatch"
+SWDIR="/tmp"
+SW="$SWDIR/stopwatch"
 NOW=$(($(date +%s%N)/1000000))
-if [ $1 == "" ]; then
-	echo "Usage: stopwatch.sh name start (e.g., stopwatch \"Test Stopwatch\" start): Starts the counter from zero."
-	echo "stopwatch.sh name lap (e.g., stopwatch \"Test Stopwatch\" lap): Marks a lap and displays all laps. Can be used multiple times."
-	echo "stopwatch.sh name # (e.g., stopwatch \"Test Stopwatch\" 60 ): Displays \"yes\" and returns 0 if specified number of seconds has elapsed."
-	echo "   Displays \"no\" and returns 1 if specified number of seconds has not elapsed."
+
+if [ -z "$1" ]; then
+	echo "Usage: "
+	echo "stopwatch.sh name start (e.g., stopwatch \"Test Stopwatch\" start):" 
+	echo "          Starts the counter from zero."
+	echo "stopwatch.sh name lap (e.g., stopwatch \"Test Stopwatch\" lap):"
+	echo "          Marks a lap and displays all laps. Can be used multiple times."
+	echo "stopwatch.sh name # (e.g., stopwatch \"Test Stopwatch\" 60 ):"
+	echo "          Displays \"yes\" and returns 0 if specified number of seconds has elapsed."
+	echo "          Displays \"no\" and returns 1 if specified number of seconds has not elapsed."
 	exit 2
 	fi
-if [ $2 == "start" ]; then
+if [ "$2" == "start" ]; then
 	mkdir -p $SWDIR
 	echo $NOW > $SW-$1
 	echo "Started."
 	exit 0
 	fi
-if [ $2 == "lap" ]; then
+if [ "$2" == "lap" ]; then
 	mkdir -p $SWDIR
 	echo $NOW >> $SW-$1
+	LAPNUM=1
 	while LAP= read -r line
 	do
-		echo $line
+#		read THEN < $SW-$1
+		let ELAPSED=NOW-LAP
+		let MILLISECONDS=ELAPSED%1000
+		let QUOTIENT=(ELAPSED-MILLISECONDS)/1000
+		let SECONDS=QUOTIENT%60
+		let QUOTIENT=(QUOTIENT-SECONDS)/60
+		let MINUTES=QUOTIENT%60
+		let QUOTIENT=(QUOTIENT-MINUTES)/60
+		let HOURS=QUOTIENT%24
+		let DAYS=(QUOTIENT-HOURS)/24
+		printf "Lap %d: %d %02d:%02d:%02d.%03d" $LAPNUM $DAYS $HOURS $MINUTES $SECONDS $MILLISECONDS
+		let LAPNUM++
 	done < $SW-$1
 	exit 0
 	fi
 
-LET ELAPSED=NOW-$(read -r line < $SW-$1)
-echo $ELAPSED
+if [ -f $SW-$1 ]; then
+	read THEN < $SW-$1
+	let ELAPSED=NOW-THEN
+	let MILLISECONDS=ELAPSED%1000
+	let QUOTIENT=(ELAPSED-MILLISECONDS)/1000
+	let SECONDS=QUOTIENT%60
+	let QUOTIENT=(QUOTIENT-SECONDS)/60
+	let MINUTES=QUOTIENT%60
+	let QUOTIENT=(QUOTIENT-MINUTES)/60
+	let HOURS=QUOTIENT%24
+	let DAYS=(QUOTIENT-HOURS)/24
+	printf "%d %02d:%02d:%02d.%03d" $DAYS $HOURS $MINUTES $SECONDS $MILLISECONDS
+	fi
+
+echo "Stopwatch $1 is not running."
+exit 2
