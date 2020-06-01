d814fa7f06e0b626ff2dd8cf427af5a01d637715
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-06-01 18:47:21 -0500

    fixing setup

--- sbin/make-letsencrypt-p12-files.sh
+++ sbin/make-letsencrypt-p12-files.sh
@@ -9,7 +9,14 @@ source $PROGRAM_DIRECTORY/source.sh
 
 letsencrypt_live_directory="/etc/letsencrypt/live"
 
-for d in $letsencrypt_live_directory
-do
-	echo "Hit on $letsencrypt_live_directory"
+for d in $letsencrypt_live_directory/*
+	do
+		if [ -d $d ]; then
+			cd $d
+			for x in cert chain fullchain privkey
+				do
+					echo "Working with $d/$x.pem"
+					openssl pkcs12 -export -inkey privkey.pem -in $x.pem -out $x.p12
+				done
+		fi
 done
--- sbin/setup/setup-os.sh
+++ sbin/setup/setup-os.sh
@@ -69,7 +69,10 @@ enable_ufw=true
 
 work_directory=$(pwd)
 running_directory=$(dirname $0)
-configs_directory="$running_directory/configs"
+git_directory="/var/local/git"
+go_directory="$git_directory/go"
+configs_directory="$go_directory/configs"
+mkdir -p /home/$USER_ME/.ssh
 chown -R $USER_ME:$USER_ME /home/$USER_ME/.ssh
 
 if [[ $NAME = "Ubuntu" ]]; then
@@ -88,6 +91,7 @@ fi
 apt-get update && apt -y dist-upgrade && apt -y clean && apt -y autoremove
 
 if ! [ -x "$(command -v apg)" ]; then
+  echo "Installing apg"
   apt-get install -y apg
 fi
 
@@ -127,7 +131,8 @@ chown root:root /etc/hostname
 # Make $USER_ME and $USER_UBUNTU users and give them sudo access and ssh access
 useradd $USER_ME
 # useradd $USER_UBUNTU
-echo $PASSWORD_ME | passwd --stdin $USER_ME
+# TWO PROBLEMS WITH THE COMMANDS BELOW: The --stdin flag doesn't work in this version of Linux, and my password is going to be dropped anyway.
+# echo $PASSWORD_ME | passwd --stdin $USER_ME
 # echo $PASSWORD_UBUNTU | passwd --stdin $USER_UBUNTU
 usermod -aG sudo $USER_ME
 # usermod -aG sudo $USER_UBUNTU
@@ -143,15 +148,19 @@ systemctl restart sshd
 # echo Users $USER_ME and $USER_UBUNTU installed and configured for ssh.
 # echo User $USER_ME installed and configured for ssh.
 
+echo Configure sudo $USER_ME and $USER_UBUNTU as sudo users
 echo "$USER_ME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/lane-NOPASSWD-users
 echo "$USER_UBUNTU ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/lane-NOPASSWD-users
+echo Removing passwords for $USER_ME and $USER_UBUNTU
 chmod 440 /etc/sudoers.d/lane-NOPASSWD-users
 passwd -d $USER_ME
+# This probably will fail, and that's fine.'
 passwd -d $USER_UBUNTU
 echo Passwords removed for $USER_ME and $USER_UBUNTU
 
 # Add custom application definitions for ufw
-cp $PROGRAM_DIRECTORY/configs/lane-applications /etc/ufw/applications.d/
+echo "Adding custom application definitions for ufw"
+cp $configs_directory/lane-applications /etc/ufw/applications.d/
 chown root:root /etc/ufw/applications.d/lane-applications
 chmod 644 /etc/ufw/applications.d/lane-applications
 ufw app update lane-applications
@@ -159,89 +168,109 @@ echo Special LANE applications installed to ufw.
 
 # CRITICAL: Install this first.
 if $install_curl ; then
+  echo "Installing curl"
   apt-get install -y curl
   echo curl installed.
   fi
 
 # Install this early.
 if $install_unzip ; then
+  echo "Installing unzip"
   apt-get install -y unzip
   echo unzip installed.
   fi
 
 # Install this early.
 if $install_wget ; then
+  echo "Installing wget"
   apt-get install -y wget
   echo wget installed.
   fi
 
 if $install_apache2 ; then
-  apt-get install -y apache2
+  echo "Installing apache2"
+  apt-get install -y apache2 apach
+  e2-doc apache2-suexec-pristine
   ufw allow 'Apache'
   ufw allow 'Apache Full'
   ufw allow http
   ufw allow https
   a2enmod ssl rewrite
+  systemctl restart apache2
   echo apache2 installed.
   fi
 
 if $install_fail2ban ; then
-  apt-get install -y fail2ban
+  echo "Installing fail2ban"
+  apt-get install -y fail2ban mailx monit sqlite3 python-pyinotify-doc
   echo fail2ban installed.
   fi
 
 if $install_net_tools ; then
+  echo "installing net_tools"
   apt-get install -y net-tools
   echo net-tools installed.
   fi
 
 if $install_openssl ; then
-  apt-get install -y openssl libcurl4-openssl-dev libssl-dev
-  echo openssl installed.
+  echo "Installing openssl"
+  apt-get install -y openssl libcurl4-openssl-dev libssl-dev libcurl4-doc libidn11-dev libkrb5-dev libldap2-dev librtmp-dev libssh2-1-dev zlib1g-dev libssl-doc
+  echo "openssl installed."
   fi
 
 if $install_php ; then
+  echo "Installing php"
   apt-get install -y php libapache2-mod-php
   apt-get install -y php-mysql php-gd php-curl php-imap php-ldap
   apt-get install -y libmcrypt-dev php-mbstring
   apt-get install -y php-dev php-pear
+  apt-get install -y libc-client2007e mlock php-curl php-gd php-imap php-ldap php-mysql uw-mailutils
   phpenmod gd curl imap ldap mbstring
   systemctl restart apache2
-  echo php installed.
+  echo "php installed."
   fi
 
 if $install_sysbench ; then
+  echo "Installing sysbench"
   apt-get install -y sysbench
-  echo sysbench installed.
+  echo "sysbench installed."
   fi
 
 if $install_tasksel ; then
+  echo "Installing tasksel"
   apt-get install -y tasksel
-  echo tasksel installed.
+  echo "tasksel installed."
   fi
 
 if $install_xrdp ; then
+  echo "Installing xrdp"
   apt-get install -y xrdp
   ufw allow from 192.168.1.0/24 to any port 3389 proto tcp
-  echo xrdp installed.
+  echo "xrdp installed."
   fi
 
 if $enable_certbot ; then
+  echo "Installing certbot"
   apt install -y certbot python3-certbot-apache
-  echo certbot installed.
+  echo "certbot installed."
   fi
 
 if $install_mailutils ; then
+  echo "installing mailutils"
   debconf-set-selections <<< "postfix postfix/relayhost $RELAYHOST"
   debconf-set-selections <<< "postfix postfix/mailname string $MAILNAME"
   debconf-set-selections <<< "postfix postfix/main_mailer_type string $MAIN_MAILER_TYPE"
   apt-get install -y mailutils
   ufw allow mail
+  echo "mailutils installed."
   fi
 
 if $install_mysql_server ; then
+  echo "Installing mysql server"
 ###### EXTREMELY IMPORTANT: Edit /etc/mysql/mysql.conf.d/mysqld.cnf and open up bind-address * ###########
   apt-get install -y openssl libcurl4-openssl-dev libssl-dev
+  apt-get install -y php-dbase php-libsodium php-mcrypt php-gmp php-symfony-service-implementation php-imagick php-twig-doc php-mcrypt
+  apt-get install -y php-symfony-translation php-recode php-gd2 php-pragmarx-google2fa php-bacon-qr-code php-samyoul-u2f-php-server
   apt-get install -y mysql-server
   ufw allow mysql
   mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
@@ -258,10 +287,12 @@ if $install_mysql_server ; then
   cp "$MYSQL_SERVER_BIN_DIR/client-cert.pem" "$MYSQL_CLIENT_CERTS_DIR/$(hostname)-MySQL-client-cert.pem"
   cp "$MYSQL_SERVER_BIN_DIR/client-key.pem" "$MYSQL_CLIENT_CERTS_DIR/$(hostname)-MySQL-client-key.pem"
   chown -R $USER_ME:$USER_ME "$MYSQL_CLIENT_CERTS_DIR"
+  echo "mysql server installed"
   fi
 
 # phpMyAdmin should be installed AFTER php and MySQL
 if $install_phpmyadmin ; then
+  echo "Instlling phpmyadmin"
   # Based on https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-phpmyadmin-on-ubuntu-20-04
   debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
   debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
@@ -277,19 +308,23 @@ if $install_phpmyadmin ; then
 #  chown -R www-data:www-data /var/www/html/.htaccess
 #  chmod 644 /var/www/html/.htaccess
   systemctl restart apache2
+  echo "phpmyadmin installed."
   fi
 
 if $install_plexmediaserver ; then
 # CAUTION: NOT TESTED FOR UBUNTU 20
+  echo "Installing Plex Media Server"
   rm /etc/apt/sources.list.d/plexmediaserver.list
   deb https://downloads.plex.tv/repo/deb public main | tee /etc/apt/sources.list.d/plexmediaserver.list
   curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
   apt update
   apt install plexmediaserver
   ufw allow plexmediaserver
+  echo "Plex Media Server installed"
   fi
 
 if $install_webmin ; then
+  echo "Installing Webmin"
   wget http://www.webmin.com/download/deb/webmin-current.deb
   apt-get install -y openssl libcurl4-openssl-dev libssl-dev
   apt-get install -y perl libnet-ssleay-perl libauthen-pam-perl \
@@ -297,25 +332,25 @@ if $install_webmin ; then
   dpkg --install webmin-current.deb
   rm webmin-current.deb
   ufw allow webmin
-  echo Webmin installed.
+  echo "Webmin installed."
   fi
 
 # Edit .vimrc settings
 touch /home/$USER_ME/.vimrc && cp /home/$USER_ME/.vimrc /home/$USER_ME/.vimrc.backup.$(date "+%Y.%m.%d-%H.%M.%S") && echo "set background=dark" > /home/$USER_ME/.vimrc && echo "set visualbell" >> /home/$USER_ME/.vimrc
-chown $USER_ME:$USER_ME /home/USER_ME/.vi*
+chown $USER_ME:$USER_ME /home/$USER_ME/.vi*
 echo .vimrc edited.
 
 echo ""
 echo "# IMPORTANT: Copy these passwords into Roboform IMMEDIATELY and reboot."
 echo "# Once you continue, these passwords cannot be recovered."
 echo ""
-echo 'PASSWORD_ME: $PASSWORD_ME'
-echo 'MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD'
-echo 'MYSQL_ADMIN_NAME: $MYSQL_ADMIN_NAME'
-echo 'MYSQL_ADMIN_PASSWORD: $MYSQL_ADMIN_PASSWORD'
-echo 'PHPMYADMIN_APP_PASS: $PHPMYADMIN_APP_PASS'
-echo 'PHPMYADMIN_ROOT_PASS: $PHPMYADMIN_ROOT_PASS'
-echo 'PHPMYADMIN_APP_DB_PASS: $PHPMYADMIN_APP_DB_PASS'
+echo "PASSWORD_ME: $PASSWORD_ME"
+echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
+echo "MYSQL_ADMIN_NAME: $MYSQL_ADMIN_NAME"
+echo "MYSQL_ADMIN_PASSWORD: $MYSQL_ADMIN_PASSWORD"
+echo "PHPMYADMIN_APP_PASS: $PHPMYADMIN_APP_PASS"
+echo "PHPMYADMIN_ROOT_PASS: $PHPMYADMIN_ROOT_PASS"
+echo "PHPMYADMIN_APP_DB_PASS: $PHPMYADMIN_APP_DB_PASS"
 echo ""
 
 echo Done.
