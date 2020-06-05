f0494c0b09180a86cc15e1b7e8e816d2201c53c4
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-06-04 23:31:02 -0500

    test.sh

---
+++ sbin/setup/install-utils.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# install-utils.sh
+# Revised 2020-06-04
+# PURPOSE: Install uncomplicated, no-down-side utilities.
+
+# Include header file
+PROGRAM_DIRECTORY=$(dirname $0)
+source $PROGRAM_DIRECTORY/../source.sh
+
+function install() {
+   logger Installing %1
+   apt-get install -y %1 %2 %3 %4 %5 %6
+}
+
+# Get to work
+install ufw
+install apg
+install curl
+install fail2ban
+install filezilla
+install gimp
+install git && ufw allow git
+install net-tools
+install openssl
+install openvpn bridge-utils
+install ssh && ufw allow ssh
+install sysbench
+install tasksel
+install unzip
+install wget
+install xrdp
+
+ufw allow http
+ufw allow https
---
+++ sbin/setup/testt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# test.sh
+# Revised 2020-06-04
+# PURPOSE: Install uncomplicated, no-down-side utilities.
+
+# Include header file
+PROGRAM_DIRECTORY=$(dirname $0)
+source $PROGRAM_DIRECTORY/../source.sh
+
+setup/instill-utils.sh
+df
