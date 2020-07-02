fa2f9d7d0b3a4e41b4c03a325ab491822f4e4677
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-07-02 16:23:36 -0500

    More lsyncd updates

--- configs/Lsyncd/oz/lsyncd.conf.lua
+++ configs/Lsyncd/oz/lsyncd.conf.lua
@@ -1,13 +1,12 @@
 settings {
-   logfile = "/var/log/lsyncd/lsyncd.log",
-   statusFile = "/var/log/lsyncd/lsyncd.status",
-   statusInterval = 20,
-   nodaemon   = false
+	logfile = "/var/log/lsyncd/lsyncd.log",
+	statusFile = "/var/log/lsyncd/lsyncd.status",
+	statusInterval = 20,
+	nodaemon = false
 }
 
 sync {
-   default.rsyncssh,
-   source = "/home/lhensley/Videos/HandBrake-Completed/",
-   host = "nuc01.lanehensley.org",
-   targetdir = "/mnt/cloteal/Plex/Pool02-1TB/in"
+	default.rsyncssh,
+	source = "/home/lhensley/Videos/HandBrake-Completed",
+	target = "nuc01.lanehensley.org:/mnt/cloteal/Plex/Pool02-1TB/in"
 }
