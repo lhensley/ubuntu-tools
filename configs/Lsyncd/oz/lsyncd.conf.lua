5cc947c7f4ced08116488b0b84ce876d185e72a5
Lane Hensley <lane.hensley@alumni.duke.edu>
2020-07-02 19:55:08 -0500

    lsyncd fix

--- configs/Lsyncd/oz/lsyncd.conf.lua
+++ configs/Lsyncd/oz/lsyncd.conf.lua
@@ -8,5 +8,5 @@ settings {
 sync {
 	default.rsyncssh,
 	source = "/home/lhensley/Videos/HandBrake-Completed",
-	target = "nuc01.lanehensley.org:/mnt/cloteal/Plex/Pool02-1TB/in"
+	target = "lhensley@nuc01.lanehensley.org:/mnt/cloteal/Plex/Pool02-1TB/in/"
 }
