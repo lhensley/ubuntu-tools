6f74bc93186e848d47cfad9bccad04b9fa85e90f
Lane Hensley <lane@lanehensley.org>
2024-05-15 07:48:36 -0500

    dump old

--- configs/etc/hosts.go
+++
@@ -1,7 +0,0 @@
-127.0.0.1 localhost
-# 192.168.168.3 nuc01.hogheavendsm.local nuc01.lan nuc01.local nuc01.lanehensley.org lanehensley.org nuc01 nuc01-wired
-# 192.168.168.8 go.hogheavendsm.local go.lan go.local go.lanehensley.org go go-wired
-# 192.168.168.70 nuc01-wifi
-# 192.168.168.251 go-wifi
-
-::1 localhost
